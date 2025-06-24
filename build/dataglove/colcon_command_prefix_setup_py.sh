6c82207052653c1d9fee6e082f9061da7e29f273
LucaPalumbo <luca183.palumbo@gmail.com>
2025-06-24 23:47:57 +0200

    Rimuove cartelle build/, install/, log/ dalla repository

--- build/dataglove/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/feld/ros2_ws/install/dataglove_msgs/share/dataglove_msgs/package.sh"
--- build/dataglove_msgs/rosidl_generator_py/dataglove_msgs/msg/__init__.py
+++
@@ -1,1 +0,0 @@
-from dataglove_msgs.msg._vmg30_data import VMG30Data  # noqa: F401
